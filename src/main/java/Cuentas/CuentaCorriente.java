c4e098fb32e061d59e566f765595b694a2bfbd7d
Alexis Jacob <ferjacobka@gmail.com>
2026-07-19 08:33:54 -0400

    Chore: se agrego metodo abonar y cargar (aun en construccion)

--- src/main/java/Cuentas/CuentaCorriente.java
+++ src/main/java/Cuentas/CuentaCorriente.java
@@ -3,19 +3,31 @@ package Cuentas;
 public class CuentaCorriente {
 
     // atributos
+    // punto 1.1
     private String titular;
     private Integer saldo; // permite valores null
     private String numero;
 
+    // Punto 1.2
     public CuentaCorriente(String titular, String numero) {
         this.titular = titular;
-        this.saldo = 0;
+        this.saldo = 0; // saldo opcional
         this.numero = numero;
     }
 
+    // saldo inicilizado por el usuario
+    public CuentaCorriente(String titular, String numero, Integer saldo) {
+        this.titular = titular;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    // Punto 1.3
     @Override
     public String toString() {
-        return "Cuenta Corriente : " + "Titular = '" + this.titular + '\'' + ", Número = '" + this.numero + '\'' + '}';
+        return (
+            "CuentaCorriente{ Titular = '" + titular + "', Numero de cuenta = '" + numero + "', Saldo = $" + saldo + "}"
+        );
     }
 
     public String getTitular() {
@@ -40,5 +52,18 @@ public class CuentaCorriente {
 
     public void setNumero(String numero) {
         this.numero = numero;
+    }
+
+    public int abonar(int sumar) {
+        if (sumar >= 0) {
+            System.out.println("Ingrese una cantidad valdia para abonar");
+            return 0;
+        } else {
+            return sumar;
+        }
+    }
+
+    public int cargar(int restar) {
+        return restar;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,8 @@ public class Main {
     static void main() {
         CuentaCorriente cuentas = new CuentaCorriente("Alexis Jacob", "0000141414");
         System.out.println(cuentas.toString());
+
+        cuentas.setSaldo(1200);
+        System.out.println(cuentas.toString());
     }
 }
